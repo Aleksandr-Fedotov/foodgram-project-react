c04519ad6920d6c0a2502e22aa53fae8def41b04
Алексндр Федотов <qwfwr2@yandex.ru>
2022-10-18 00:08:26 +0300

    last

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -91,9 +91,9 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.environ.get('DB_NAME', 'newbd'),
-        'USER': os.environ.get('POSTGRES_USER', 'user2'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '123'),
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '5432')
     }
