50c1560be64250e4b0d1ee6b3e627a617461badc
Алексндр Федотов <qwfwr2@yandex.ru>
2022-10-21 20:20:20 +0300

    newserv

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -101,6 +101,7 @@ DATABASES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
