bc6496206f8d9bcfac5047cf7ae8c98a1b06c0a3
Алексндр Федотов <qwfwr2@yandex.ru>
2022-10-29 03:44:14 +0600

    mist_aggr

--- backend/api/services.py
+++ backend/api/services.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 
 
-def get_ingredients_for_shopping(ingredients):
-    shopping_cart = '\n'.join([
-        f'{ingredient["ingredient__name"]} - {ingredient["total_amount"]} '
-        f'{ingredient["ingredient__measurement_unit"]}'
-        for ingredient in ingredients])
-    filename = 'shopping_list.txt'
-    response = HttpResponse(shopping_cart, content_type='text/plain')
-    response['Content-Disposition'] = f'attachment; filename={filename}'
+def get_ingredients_for_shopping(list):
+    shopping_list = []
+    for item in list:
+        shopping_list.append(
+            f'{item} - {list[item]["amount"]} '
+            f'{list[item]["measurement_unit"]} \n'
+        )
+    response = HttpResponse(shopping_list, 'Content-Type: text/plain')
+    response['Content-Disposition'] = (
+        'attachment; filename="shopping_list.txt"'
+    )
     return response
