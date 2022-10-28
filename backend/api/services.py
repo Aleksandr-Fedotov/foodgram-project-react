f99de2bc124e6c32e83041dda759675bbfbfd2fb
Алексндр Федотов <qwfwr2@yandex.ru>
2022-10-29 03:46:24 +0600

    final

--- backend/api/services.py
+++ backend/api/services.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 
 
-def get_ingredients_for_shopping(list):
+def get_ingredients_for_shopping(ingredients):
     shopping_list = []
-    for item in list:
+    for item in ingredients:
         shopping_list.append(
-            f'{item} - {list[item]["amount"]} '
-            f'{list[item]["measurement_unit"]} \n'
+            f'{item} - {ingredients[item]["amount"]} '
+            f'{ingredients[item]["measurement_unit"]} \n'
         )
     response = HttpResponse(shopping_list, 'Content-Type: text/plain')
     response['Content-Disposition'] = (
